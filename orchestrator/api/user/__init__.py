9cbb9a51759abf4772b6582e1c78ab019e72f7dd
Poco <61045140+Loco-CTO@users.noreply.github.com>
2025-01-15 22:14:33 +0000

    fix: update user API namespace to use Flask-RestX for improved clarity

--- orchestrator/api/user/__init__.py
+++ orchestrator/api/user/__init__.py
@@ -3,8 +3,8 @@
 ### Copyright 2019-2024 © Rystal. All Rights Reserved.
 ### ======================================================================
 
-from .authenticate import api_namespace_user_authenticate
+from flask_restx import Namespace
 
-api_namespace_user = [
-    api_namespace_user_authenticate,
-]
+api_namespace_user = Namespace("User", description="User related operations")
+
+from . import me
