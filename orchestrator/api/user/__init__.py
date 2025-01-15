f57a5db247fdcd4c78a156674f7af2bef036bb1a
Poco <61045140+Loco-CTO@users.noreply.github.com>
2025-01-15 21:02:20 +0000

    feat: add user authentication API namespace to orchestrator

---
+++ orchestrator/api/user/__init__.py
@@ -0,0 +1,10 @@
+### ======================================================================
+### ZenStream Orchestrator
+### Copyright 2019-2024 © Rystal. All Rights Reserved.
+### ======================================================================
+
+from .authenticate import api_namespace_user_authenticate
+
+api_namespace_user = [
+    api_namespace_user_authenticate,
+]
