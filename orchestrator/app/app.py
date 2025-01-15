621fd1f828e49d11720c876e4e99584193381a8f
Poco <61045140+Loco-CTO@users.noreply.github.com>
2025-01-15 21:31:17 +0000

    feat: implement ZenStream Orchestrator with API and logging functionality

---
+++ orchestrator/app/app.py
@@ -0,0 +1,75 @@
+### ======================================================================
+### ZenStream Orchestrator
+### Copyright 2019-2024 © Rystal. All Rights Reserved.
+### ======================================================================
+
+import os
+
+from waitress import serve
+from flask import Blueprint, Flask
+from flask_restx import Api, Resource, fields
+from logger import Logger
+from .config import load_config
+
+from api import api_namespaces
+
+
+class Orchestrator:
+    def __init__(self, logger: Logger = None, version: str = None):
+        """
+        Initialize the Orchestrator.
+
+        Args:
+            logger (Logger): The logger instance.
+            version (str): The version of the Orchestrator.
+        """
+        load_config()
+        self.logger = logger
+        self.version = version
+
+    def create(self):
+        """
+        Create the Orchestrator.
+        """
+        self.logger.info("Creating Orchestrator...")
+        self.app = Flask(__name__)
+
+        if os.getenv("SECRET_KEY") is None:
+            raise Exception("Environment variable `SECRET_KEY` not set")
+
+        self.app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+        api_blueprint = Blueprint("api", __name__, url_prefix="/api")
+
+        self.api = Api(
+            api_blueprint,
+            authorizations={
+                "token": {"type": "apiKey", "in": "header", "name": "TOKEN"},
+                "apiKey": {"type": "apiKey", "in": "header", "name": "X-API-KEY"},
+            },
+            security="apiKey",
+            version=self.version,
+            title="ZenStream API",
+            description="ZenStream Orchestrator Authentication API",
+            doc="/swagger/",
+        )
+
+        for api_namespace in api_namespaces:
+            for item in api_namespace:
+                self.api.add_namespace(item, "/")
+                self.logger.info(f"Registered API namespace: {item.name}")
+
+        self.app.register_blueprint(api_blueprint)
+
+        self.serve()
+
+    def serve(self):
+        """
+        Serve the Orchestrator.
+        """
+        if os.getenv("DEBUG"):
+            self.logger.info("Serving Orchestrator in debug mode...")
+            self.app.run(debug=True, host="localhost", port=5090, use_reloader=False)
+        else:
+            self.logger.info("Serving Orchestrator...")
+            serve(self.app, host="0.0.0.0", port=5090)
