80d91ed089f5a4bf6b8ceeec85d406294b138294
anson <111585144+neonplayanson@users.noreply.github.com>
2025-02-06 22:46:02 +0000

    Update Greeting component user prop to use generic placeholder

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -13,7 +13,7 @@ import {
 export default function Dashboard() {
   return (
     <div className="w-full h-screen bg-schemes-dark-surfaceContainerLowest items-start justify-start">
-      <Greeting user="anson" />
+      <Greeting user="user" />
       <div className="grid grid-cols-1 md:grid-cols-2 items-start justify-center pt-16">
         <div className="flex flex-col items-start justify-start w-full h-full gap-9 singlecol-padding md:leftcol-padding">
           <Gadget
