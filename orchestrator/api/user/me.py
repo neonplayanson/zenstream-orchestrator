12ca6506935d81343a6bd9e35fd5d84daee63b6e
anson <111585144+neonplayanson@users.noreply.github.com>
2025-02-13 21:42:29 +0000

    Fixed /user/me endpoint

--- orchestrator/api/user/me.py
+++ orchestrator/api/user/me.py
@@ -12,22 +12,24 @@ from . import api_namespace_user
 @api_namespace_user.route("user/me")
 class UserAuthenticate(Resource):
     get_model = api_namespace_user.model(
-        "Get",
-        {
-            "authorized": fields.Boolean(description="Is user authorized"),
+        name="Get",
+        model={
+            "UserId": fields.String(required=True, description="The user ID"),
+            "authorized": fields.Boolean(required=True, description="Is the user authorized"),
         },
+
     )
     get_parser = reqparse.RequestParser()
     get_parser.add_argument("UserId", type=str, help="The user ID.", location="args")
 
     @authenticate
     @api_namespace_user.doc(parser=get_parser)
-    @api_namespace_user.marshal_with(get_model, description="Get the user's profile.")
+    @api_namespace_user.marshal_with(get_model, description="Get the user's profile.", code=202)
     def get(self):
         """Get the user's profile."""
         args = self.get_parser.parse_args()
 
         user_id = args.get("UserId")
-
         is_authorized = random.choice([True, False])
-        return {"UserId": user_id, "authorized": is_authorized}
+
+        return {"UserId": user_id, "authorized": is_authorized}, 202
--- orchestrator/utils/wrappers.py
+++ orchestrator/utils/wrappers.py
@@ -5,12 +5,16 @@
 
 from functools import wraps
 
-
 def authenticate(func):
     """Authenticate decorator"""
 
     @wraps(func)
     def wrapper(*args, **kwargs):
         """Wrapper"""
+        #todo: Implement authentication logic
+        user_authenticated = True
+        if not user_authenticated:
+            return {"message": "User is not authenticated to this action"}, 401
+        return func(*args, **kwargs)
 
     return wrapper
