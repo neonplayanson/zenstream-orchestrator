d52f3fe64691e7d5c92bc4523ab19be4eb054912
anson <111585144+neonplayanson@users.noreply.github.com>
2025-02-23 22:42:58 +0000

    fix: add invite validation to user registration process

--- orchestrator/api/user/register.py
+++ orchestrator/api/user/register.py
@@ -1,8 +1,9 @@
 from . import api_namespace_user
-from flask_restx import Resource, fields, reqparse
+from flask_restx import Resource, reqparse
 from app.config import Config
 from hashlib import sha256
-
+from flask import request
+from urllib.parse import urlparse
 
 @api_namespace_user.route("user/register")
 class UserRegister(Resource):
@@ -17,6 +18,7 @@ class UserRegister(Resource):
 
     @api_namespace_user.doc(parser=get_parser)
     @api_namespace_user.response(201, "Registered the user.")
+    @api_namespace_user.response(403, "Invite invalid")
     @api_namespace_user.response(409, "Failed to register the user.")
     @api_namespace_user.response(500, "An error occurred while registering.")
     def post(self):
@@ -26,8 +28,14 @@ class UserRegister(Resource):
         args = self.get_parser.parse_args()
         username = args.get("Username").strip()
         password = sha256(args.get("Password").strip().encode()).hexdigest()
-        print(args.get("url"))
+        invite_id = urlparse(request.headers.get("Referer")).path.split("/")[-2]
+
         db = Config()._database
+
+        check = db.execute("SELECT * FROM invites WHERE url = ?", (invite_id,))
+        if not check:
+            return {}, 403
+        
         try:
             db.execute("INSERT INTO users VALUES (?, ?, '{}')", (username, password))
             return {}, 201
